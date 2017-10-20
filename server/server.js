dab019ea8266bc908045f8ee23152c9af3716cd8
iuriifaevskii <urfaevskii@gmail.com>
2017-10-21 01:22:21 +0300

    add GET /todos and /todos/id route

--- server/server.js
+++ server/server.js
@@ -21,6 +21,23 @@ app.post('/todos', (req, res) => {
     });
 });
 
+app.get('/todos', (req, res) => {
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, e => {
+        res.status(400).send(e);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+    var { id } = req.params;
+    Todo.findById(id).then((todo) => {
+        res.send({todo});
+    }, e => {
+        res.status(400).send(e);
+    });
+});
+
 app.listen(3000, 'localhost', () => {
     console.log('Started on port 3000');
 });
