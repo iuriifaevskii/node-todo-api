a4d801b7aa67bd60083abdd0131bf0a4cddde7a8
iuriifaevskii <urfaevskii@gmail.com>
2017-10-21 16:05:28 +0300

    add patch todos/id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+const _ = require('lodash');
 var express = require('express');
 var bodyParser = require('body-parser');
 var { ObjectID } = require('mongodb');
@@ -78,6 +79,42 @@ app.delete('/todos/:id', (req, res) => {
         return res.status(200).send("good");
     }).catch((e) => {
         res.status(400).send(e);
+    });
+});
+//in postman body
+/*
+{
+	"completed": false,
+	"text": "update all"
+}
+or
+{
+	"completed": true,
+}
+*/
+app.patch('/todos/:id', (req, res) => {
+    var { id } = req.params;
+    var body = _.pick(req.body, ['text', 'completed']);
+    console.log(body);
+
+    if(!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    if(_.isBoolean(body.completed) && body.completed) {
+        body.completedAt = new Date().getTime();
+    } else {
+        body.completed = false;
+        body.completedAt = null;
+    }
+
+    Todo.findByIdAndUpdate(id, {$set: body }, {new: true}).then(todo => {
+        if(!todo) {
+            return res.status(400).send();
+        }
+        res.send({todo});
+    }).catch(e => {
+        res.status(400).send();
     });
 });
 
