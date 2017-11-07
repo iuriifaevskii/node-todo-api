56d94745f9fe7db1080b2ad1b11fb0d6baa55ff1
iuriifaevskii <urfaevskii@gmail.com>
2017-11-07 20:51:39 +0200

    create and setup env config

---
+++ server/config/config.js
@@ -0,0 +1,14 @@
+//config.json -> .gitignore
+
+var env = process.env.NODE_ENV || 'development';
+
+if (env === 'development' || env === 'test') {
+    var config = require('./config.json');
+    var envConfig = config[env];
+
+    Object.keys(envConfig).forEach((key) => {
+        process.env[key] = envConfig[key];
+    });
+}
+
+module.exports = env;
